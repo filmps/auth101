e9d17bce6af260e5d1ec85f87381881e915171e0
filmps <97680543+filmps@users.noreply.github.com>
2024-03-12 23:23:35 -0700

    a

--- WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
+++ WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735263a4121c8a6268dc8e96e3986efbb96d2fdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9d6e570ce6486156db05ba7029d179b32f648e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
